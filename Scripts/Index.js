d74201b4569ede34c314727dd1c13b05c6ac81c5
QuiteMuffin <86913571+QMuffinBoy@users.noreply.github.com>
2022-06-25 15:59:44 +0800

    Add files via upload

---
+++ Scripts/Index.js
@@ -0,0 +1,11 @@
+function AboutMeButton_OnClick(){
+    document.location.href ="About.html";
+}
+
+function ListNames_Button_OnClick(){
+    document.location.href ="ListNamesPage.html";
+}
+
+function logo_onCLick(){
+    document.location.href ="index.html"
+}
